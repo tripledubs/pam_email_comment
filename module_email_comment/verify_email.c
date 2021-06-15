5161222ef6292ca52a715492391c19d9493fa085
Your Name <you@example.com>
2021-06-14 22:49:56 -0500

    use openpam conversationf function

--- module_email_comment/verify_email.c
+++ module_email_comment/verify_email.c
@@ -14,12 +14,12 @@
 #include <readline/readline.h>
 
 #ifdef DEBUG
-    #define D if (1)
+#define D if (1)
 #else
-    #define D if (0)
+#define D if (0)
 #endif
 
-bool confirm (char *answer) {
+bool confirm(char *answer) {
     printf("Does this look correct? '%s'\n", answer);
     char confirm = getchar();
     if (confirm == 'Y' || confirm == 'y') {
@@ -41,16 +41,20 @@ int hasAtChar(char *string) {
     return found;
 }
 
+int getDomain
+
 int print_pwent(struct passwd *pw) {
     printf("Name: %s\n", pw->pw_gecos);
 }
 
-PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char
-        **argv) {
+PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t * pamh,
+                                int flags, int argc, const char **argv) {
     struct passwd *pw = getpwuid(getuid());
-    if ( !hasAtChar(pw->pw_gecos) ) {
+    if (!hasAtChar(pw->pw_gecos)) {
         printf("It looks like we do not have your e-mail on file\n");
         char *email = readline("Please enter your e-mail address. ");
+    } else {
+        printf("We have your e-mail on file, thank you.");
     }
     return PAM_SUCCESS;
 }
